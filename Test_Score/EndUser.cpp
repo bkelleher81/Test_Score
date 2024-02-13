ae5a9ce070db30e7c4f25756b553400e0b1477da
bkelleher81 <bkelleher127@students.mchenry.edu>
2024-02-13 16:51:27 -0600

    Add project files.

---
+++ Test_Score/EndUser.cpp
@@ -0,0 +1,9 @@
+#include"Test_Score.h"
+
+int main()
+{
+	Test_Score h;
+	h.showGrade();
+
+		return 0;
+}
---
+++ Test_Score/Test_Score.h
@@ -0,0 +1,11 @@
+#pragma once
+class Test_Score
+{
+private:
+
+public:
+	void showGrade();
+
+};
+
+
